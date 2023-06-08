1a84893e3ddc01c5c1287f582f52877e92974010
jon_prog <jonashenri.aquino@gmail.com>
2023-06-08 20:03:52 -0300

    rename columm of tabela contact

--- contact/admin.py
+++ contact/admin.py
@@ -4,7 +4,7 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
-    ordering = '-id'
+    ordering = '-id',
     # list_filter = 'created_date'
     search_fields = 'id', 'first_name',
     list_per_page = 10
--- contact/models.py
+++ contact/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class Contact(models.Model):
-    firt_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
-    created_date = models.DateField(default=timezone.now) 
+    created_date = models.DateTimeField(default=timezone.now) 
     description = models.TextField(blank=True)
 
     def __str__(self) -> str:
-        return f'{self.firt_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
