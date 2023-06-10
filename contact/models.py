7429eee8e03c04ecc0f18cbc569d1999b47477a9
jon_prog <jonashenri.aquino@gmail.com>
2023-06-10 18:25:36 -0300

    Add users fakers

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     class Meta:
@@ -22,6 +24,11 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(
         Category,
+        on_delete= models.SET_NULL,
+        blank=True, null= True
+    )
+    owner = models.ForeignKey(
+        User,
         on_delete= models.SET_NULL,
         blank=True, null= True
     )
