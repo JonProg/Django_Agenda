1e7863162d598200e45a72e0a6cd606df30ada49
jon_prog <jonashenri.aquino@gmail.com>
2023-06-13 06:31:57 -0300

    Add partials

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
 
     context = {
         'contacts':contacts,
+        'site_title':'Contatos -'
     }
     return render(
         request,
@@ -20,9 +21,12 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+
+    site_title = f'{single_contact.first_name} {single_contact.last_name} -'
     
     context = {
         'contact':single_contact,
+        'site_title': site_title
     }
 
     return render(
