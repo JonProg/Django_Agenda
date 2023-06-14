3e33c57f58642a42293c6cc7c7dd195482844fee
jon_prog <jonashenri.aquino@gmail.com>
2023-06-14 18:53:46 -0300

    Add fuction for save data in server

--- contact/views/contact_form.py
+++ contact/views/contact_form.py
@@ -1,16 +1,23 @@
 from contact.forms import ContactForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         context = {
-            'form':ContactForm(request.POST)
+            'form':form
         }
+
+        if form.is_valid():
+            form.save()
+            return redirect('contact:create')
+
         return render(
             request,
             'contact/create.html',
             context,
             )
+        
 
     context = {
         'form':ContactForm()
