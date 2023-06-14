be4fcbf1183bd217894dfd4c3395e1fec06af075
jon_prog <jonashenri.aquino@gmail.com>
2023-06-13 21:38:50 -0300

    Add pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,19 @@
-from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.db.models import Q
 from contact.models import Contact
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[0:10]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts':contacts,
+        'page_obj':page_obj,
         'site_title':'Contatos -'
     }
     return render(
@@ -32,9 +37,13 @@ def search(request):
             Q(phone__icontains = search_value)
         )\
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts':contacts,
+        'page_obj':page_obj,
     }
     return render(
         request,
