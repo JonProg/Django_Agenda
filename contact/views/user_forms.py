10fe7a9847af28cb19dd30ff541eb4e8f6cbd687
jon_prog <jonashenri.aquino@gmail.com>
2023-06-17 17:45:49 -0300

    Added update function for user

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages, auth
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -18,9 +18,36 @@ def register(request):
         request,
         'contact/register.html',
         {
+            'title':'Register',
             'form': form
         }
     )
+
+def user_update(request):
+    form = RegisterUpdateForm(instance = request.user)
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/register.html',
+            {
+                'title':'Update User',
+                'form':form
+            }
+        )
+    form = RegisterUpdateForm(data = request.POST, instance = request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/register.html',
+            {
+                'form':form
+            }
+        )
+
+    form.save()
+    auth.logout(request)
+    return redirect('contact:login')
 
 def login_view(request):
     form = AuthenticationForm(request)
